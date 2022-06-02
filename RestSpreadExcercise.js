79f9c700da2ad25d3c07ee328160f3696fbc3fd2
Clifforddd <90348904+Clifforddd@users.noreply.github.com>
2022-06-02 14:23:39 -0700

    Update RestSpreadExcercise.js

--- RestSpreadExcercise.js
+++ RestSpreadExcercise.js
@@ -1,1 +1,20 @@
+//Refactor it to use the rest operator & an arrow function:
+const filterOutOdds = (...rest) => {
+  return rest.filter(num => (num % 2 === 0))
+};
 
+
+//findMin
+const findMin = (...rest) => {
+  return rest.reduce((min, nextNum) => {
+  return (min < nextNum) ? min : (min = nextNum)
+  })
+};
+
+
+//mergeObjects
+function mergeObjects(x, y) {
+  return ({...x, ...y});
+}
+
+//doubleAndReturnArgs
