a8350d5409ee5500bb8fc83bd529596411b885a7
Clifforddd <90348904+Clifforddd@users.noreply.github.com>
2022-06-02 16:45:18 -0700

    Update RestSpreadExcercise.js

--- RestSpreadExcercise.js
+++ RestSpreadExcercise.js
@@ -18,3 +18,57 @@ function mergeObjects(x, y) {
 }
 
 //doubleAndReturnArgs
+const doubleAndReturnArgs = (spread, ...rest) => {
+  return [...spread, ...rest.map((num) => {
+    return (num * 2);
+  })]
+};
+
+//Slice and Dice!
+/** remove a random element in the items array
+and return a new array without that item. */
+
+const removeRandom = (items) => {
+      const random = Math.floor(Math.random() * items.length);
+      return [...items.slice(0, random), ...items.slice(random + 1)];
+}
+
+/** Return a new array with every item in array1 and array2. */
+
+const extend = (array1, array2) => {
+  return [...array1, array2];
+}
+
+/** Return a new object with all the keys and values
+from obj and a new key/value pair */
+
+function addKeyVal(obj, key, val) {
+   let newObj = {...obj}
+  newObj[key] = val;
+  return newObj;
+}
+
+
+/** Return a new object with a key removed. */
+
+const removeKey = (obj, key) => {
+  let nObj = {...obj};
+  delete nObj[key];
+  return nObj;
+}
+
+
+/** Combine two objects and return a new object. */
+
+const combine = (obj1, obj2) => {
+ return {...obj1, ...obj2};
+}
+
+
+/** Return a new object with a modified key and value. */
+
+const update = (obj, key, val) => {
+  let nObj = {...obj};
+  nObj[key] = val;
+  return nObj;
+}
